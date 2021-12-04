b7dec029cb7d67675ce1cd24acbd2c8e2a5da31b
jsas-uw <91643389+jsas-uw@users.noreply.github.com>
2021-12-04 09:23:17 -0600

    mocked up the GUI

--- Main.java
+++ Main.java
@@ -29,7 +29,8 @@ public class Main extends Application {
 
 	private static final int WINDOW_WIDTH = 1000;
 	private static final int WINDOW_HEIGHT = 800;
-	private static final String APP_TITLE = "Hello JavaFX World!";
+	private static final String APP_TITLE = "The Social Network";
+	
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -40,8 +41,26 @@ public class Main extends Application {
 		List<Button> friends = new ArrayList<Button>();
 		Button roundButton = new Button();
 		Button initButton = new Button("BS");
+		Button secondaryButton = new Button("B2");
 
         initButton.setStyle(
+                "-fx-background-radius: 5em; " +
+                "-fx-min-width: 75px; " +
+                "-fx-min-height: 75px; " +
+                "-fx-max-width: 75px; " +
+                "-fx-max-height: 75px;"
+        );
+        
+        
+        /*
+         * quick and dirty
+         * I just copied all the code 
+         * from init button to make 
+         * secondary button look the same
+         * should probably be refactored into
+         * a method....
+         */
+        secondaryButton.setStyle(
                 "-fx-background-radius: 5em; " +
                 "-fx-min-width: 75px; " +
                 "-fx-min-height: 75px; " +
@@ -76,12 +95,30 @@ public class Main extends Application {
 			friends.add(roundButton);
 		}
 
+		/*
+		 * We need to support multiselect
+		 * I'm thinking the left pane should 
+		 * have a secondary user
+		 * then left click to select primary,
+		 * right click to select secondary
+		 * search user should display a message in the status bar
+		 * Add user shouldn't clear the field
+		 * The select user/make secondary user probably should
+		 * clear the text field  
+		 */
+		
+		//Left pane
+			// vertical box to display the selected and secondary users
+        	VBox leftvbox = new VBox();
+        	Label primaryLabel = new Label("Selected User");
+        	leftvbox.getChildren().add(primaryLabel);
+        	leftvbox.getChildren().add(initButton);
+        	Label secondaryLabel = new Label("Secondary User");
+        	leftvbox.getChildren().add(secondaryLabel);
+        	leftvbox.getChildren().add(secondaryButton);
+        	
 
-		// Create a vertical box
-        	VBox vbox = new VBox();
-        	vbox.getChildren().add(initButton);
-
-		// Create a flow pane for the buttons
+		// Right: Create a flow pane of buttons for all users
 			FlowPane flow = new FlowPane();
 			flow.setVgap(8);
      		flow.setHgap(4);
@@ -97,7 +134,7 @@ public class Main extends Application {
 			uVBox.getChildren().add(new Label("All Users"));
 			uVBox.getChildren().add(flow);
 
-		// Create a flow pane for the buttons
+		// Center: Create a flow pane of buttons for the selected users friends
 			FlowPane friendsFlow = new FlowPane();
 			friendsFlow.setVgap(8);
 			friendsFlow.setHgap(4);
@@ -117,10 +154,14 @@ public class Main extends Application {
 			VBox topVBox = new VBox();
 			HBox top1HBox = new HBox();
 			HBox top2HBox = new HBox();
+			HBox top3HBox = new HBox();
+			HBox top3HBoxLabel = new HBox();
 			topVBox.getChildren().add(top1HBox);
 			topVBox.getChildren().add(top2HBox);
+			topVBox.getChildren().add(top3HBoxLabel);
+			topVBox.getChildren().add(top3HBox);
 			
-		//top line 1:
+		//top line 1: Load, Save and Setup
 			//import
 			top1HBox.getChildren().add(new Button("Import file"));
 			TextField importField = new TextField();
@@ -130,20 +171,58 @@ public class Main extends Application {
 			top1HBox.getChildren().add(new Button("Save file"));
 			TextField saveField = new TextField();
 			top1HBox.getChildren().add(saveField);
-		
-		//top line 2:
-			//friend search
+			//clear
+			//needs to remove all vertices and connections
+			top1HBox.getChildren().add(new Button("Clear Current Nework"));
+			top1HBox.getChildren().add(new Button("Show All Users"));
+			
+		//top line 2: Friend Search and related functions
 			top2HBox.getChildren().add(new Button("Search User"));
 			TextField friendField = new TextField();
 			top2HBox.getChildren().add(friendField);
 			top2HBox.getChildren().add(new Button("Add User"));
-			top2HBox.getChildren().add(new Button("Add Friend"));
-			top2HBox.getChildren().add(new Button("Find Shared Friends"));
-			top2HBox.getChildren().add(new Button("Find Shortest Path Between Users"));
+			top2HBox.getChildren().add(new Button("Select User"));
+			top2HBox.getChildren().add(new Button("Make Secondary User"));
 			
+		//top line 3 label: help text for line three
+			Label line3Helptext = new Label("The following buttons act on the seclected and secondary users: ");
+			top3HBoxLabel.getChildren().add(line3Helptext);
 			
-		//Bottom
-
+		//top line 3: help text for line three
+			top3HBox.getChildren().add(new Button("Add Friend"));
+			top3HBox.getChildren().add(new Button("Remove Friend"));
+			top3HBox.getChildren().add(new Button("Find Shared Friends"));
+			top3HBox.getChildren().add(new Button("Find Shortest Path Between Users"));
+			
+						
+		//Bottom	
+			VBox bottomVBox = new VBox();
+			HBox statusHBox = new HBox();
+			
+			//Status message
+			String statusMsg = "Status message: \nLeft click to set main user, \nRight click to set secondary";
+			/*
+			 * ^eventually this should be a method
+			 * so everytime we refresh the scene
+			 * we call getStatusMsg to get the string
+			 * to set the label text
+			 */
+			
+			Label statusLabel = new Label(statusMsg);
+			
+			//Stats
+			
+			int users = 100; //graph.order
+			int connections = 237; //graph.size
+			String userMsg = "Total Users: " + users;
+			String connectionsMsg = "\tTotal Connections: " + connections;
+			Label userLabel = new Label(userMsg);
+			Label connectionLabel = new Label(connectionsMsg);
+			statusHBox.getChildren().add(userLabel);
+			statusHBox.getChildren().add(connectionLabel);
+			bottomVBox.getChildren().add(statusLabel);
+			bottomVBox.getChildren().add(statusHBox);
+			
 		// Main layout is Border Pane example (top,left,center,right,bottom)
         	BorderPane root = new BorderPane();
 
@@ -154,10 +233,10 @@ public class Main extends Application {
 			ScrollPane fScrollPane = new ScrollPane(fVBox);
 			scrollPane.setFitToWidth(true);
 			fScrollPane.setFitToWidth(true);
-        	root.setLeft(vbox);
+        	root.setLeft(leftvbox);
 			root.setCenter(fScrollPane);
 			root.setRight(scrollPane);
-			root.setBottom(new Label("This is the root.setBottom"));
+			root.setBottom(bottomVBox);
 
 		Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
 
