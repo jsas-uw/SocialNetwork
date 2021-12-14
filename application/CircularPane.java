e72be4efbf1e4d743513bd8866c0d79a579f3435
bill-wisc <90437402+bill-wisc@users.noreply.github.com>
2021-12-13 21:30:41 -0600

    prevent divide by zero
    a little hacky, in the getChildren()-1, the -1 is because the top label is already a child of the pane.
    not proud of that

--- application/CircularPane.java
+++ application/CircularPane.java
@@ -10,7 +10,8 @@ public class CircularPane extends Pane {
     @Override
     protected void layoutChildren() {
         final int radius = 200;
-        final double increment = 360 / (getChildren().size()-1);
+        final double divisor = Math.max(getChildren().size()-1,1);  // prevent divide by zero
+        final double increment = 360.0 / divisor;
         double degrees = 0;
         for (Node node : getChildren()) {
             double x = radius * Math.cos(Math.toRadians(degrees)) + getWidth() / 2;
